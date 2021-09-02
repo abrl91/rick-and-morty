090cdfe94c89410a44b1b9c58d1ad863767b19fb
Amit Barletz <amitbarletz@outlook.com>
2021-09-02 18:12:46 +0300

    still wip

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,7 @@
 import {Fragment} from "react";
 
 const Card = ({ character }) => {
-    const { id, name, image, status, species, location } = character;
-
+    const { id, name, image, status, species, location, episode } = character;
     return <Fragment>
         <div key={id} className="card">
             <h1>{name}</h1>
@@ -10,6 +9,12 @@ const Card = ({ character }) => {
             <p>{status}</p>
             <p>{species}</p>
             <p>{location.name}</p>
+            <p>Episodes:</p>
+            <ul>
+                {episode.map(ep => (
+                    <li key={ep.id}>{ep.name}</li>
+                ))}
+            </ul>
         </div>
     </Fragment>
 }
--- src/components/DataListREST.js
+++ src/components/DataListREST.js
@@ -1,53 +1,33 @@
 import {Fragment, useCallback, useEffect, useState} from "react";
-import useFetch from "../custom-hooks/useFetch";
 import Card from "./Card";
 
 const DataListREST = () => {
     const defaultEndpoint = 'https://rickandmortyapi.com/api';
-    /*
-        character
-    */
-    const characterApi = `${defaultEndpoint}/character`;
-    const [loadingCharacter, errorCharacter, initialCharacterData] = useFetch(characterApi, {});
-    const {info: characterInfo, results: defaultCharacterResults = [] } = initialCharacterData;
-    const [characterResults, setCharacterResults] = useState(defaultCharacterResults);
+    const characterEndPoint = `${defaultEndpoint}/character`;
+    const [characterResults, setCharacterResults] = useState([]);
     const [characterPage, setCharacterPage] = useState({
-        ...characterInfo,
-        current: characterApi,
+        prev: null,
+        current: characterEndPoint,
+        next: null,
+        pages: 34,
+        count: 671,
     });
     const { current: currentCharacter } = characterPage;
+    const [characterEpisodes, setCharacterEpisodes] = useState([]);
 
-    /*
-        episode
-    */
-    const episodeApi = `${defaultEndpoint}/episode`;
-    const [loadingEpisode, errorEpisode, initialEpisodeData] = useFetch(episodeApi, {});
-    const { info: episodeInfo, results: defaultEpisodeResults = [] } = initialEpisodeData;
-    const [episodeResults, setEpisodeResults] = useState(defaultEpisodeResults);
-    const [episodePage, setEpisodePage] = useState({
-        ...episodeInfo,
-        current: episodeApi,
-    });
-    const {current: currentEpisode} = episodePage;
-
-    const request = useCallback(async (type, setPage, setResults, currentEndPoint) => {
-        const res = await fetch(currentEndPoint);
+    const request = useCallback(async () => {
+        const res = await fetch(currentCharacter);
         const nextData = await res.json();
         const { info: nextInfo, results: nextResults = [] } = nextData;
 
-        setPage(prev => {
+        setCharacterPage(prev => {
             return {
                 ...prev,
                 ...nextInfo
             }
         });
 
-        if ( !nextInfo?.prev ) {
-            setResults(nextResults);
-            return;
-        }
-
-        setResults(prev => {
+        setCharacterResults(prev => {
             return [
                 ...prev,
                 ...nextResults
@@ -55,31 +35,39 @@ const DataListREST = () => {
         });
 
 
-    }, []);
+    }, [currentCharacter]);
 
     useEffect(() => {
-        request('character', setCharacterPage, setCharacterResults, currentCharacter);
-        request('episode', setEpisodePage, setEpisodeResults, currentEpisode);
-    }, [request, currentCharacter, currentEpisode]);
+        request();
+    }, [request]);
+
+
+    useEffect(() => {
+        characterResults.map(character => {
+            const slimEpisodeArr = character.episode.slice(0, 5);
+            return Promise.all(slimEpisodeArr.map(ep => {
+                return fetch(ep)
+                    .then(res => res.json())
+            }))
+                .then(episodesRes => {
+                    const characterResultsExpended = {...character, episode: episodesRes};
+                    setCharacterEpisodes(charEpisodes => ([
+                        ...charEpisodes, characterResultsExpended
+                    ]))
+                });
+        });
+    }, [characterResults]);
 
     const handleLoadMore = () => {
         setCharacterPage(prev => (
             { ...prev, current: characterPage?.next }
         ));
-
-        setEpisodePage(prev => (
-            { ...prev, current: episodePage?.next }
-        ));
     }
 
-    const charactersData = characterResults.map((character) => (
-        <Card key={character.id} character={character}/>
-    ));
-
-
-
-    if (loadingCharacter || loadingEpisode) return <p>Loading</p>
-    if (errorCharacter || errorEpisode) return <p>Something Went Wrong</p>
+    const charactersData = characterEpisodes.map((character) => {
+        console.log(character);
+        return <Card key={character.id} character={character}/>
+    });
 
     return <Fragment>
         <div className="data-list">
