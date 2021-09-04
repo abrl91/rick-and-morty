994e005081b828df1d6889aa3b52395476fd116f
Amit Barletz <amitbarletz@outlook.com>
2021-09-04 20:32:02 +0300

    another refactoring

--- src/components/DataListREST.js
+++ src/components/DataListREST.js
@@ -1,63 +1,59 @@
-import {Fragment, useCallback, useEffect, useState} from "react";
+import {Fragment, useEffect, useState} from "react";
 import CardDataREST from "./CardDataREST";
 import classes from "./DataList.module.css";
 
 const DataListREST = () => {
-    const defaultEndpoint = 'https://rickandmortyapi.com/api';
-    const characterEndPoint = `${defaultEndpoint}/character`;
-    const [characterResults, setCharacterResults] = useState([]);
-    const [characterPage, setCharacterPage] = useState({
-        prev: null,
-        current: characterEndPoint,
-        next: null,
-        pages: 34,
-        count: 671,
-    });
-    const { current: currentCharacter } = characterPage;
+    const baseApiEndPoint = 'https://rickandmortyapi.com/api';
+    const [page, setPage] = useState(1);
+    const [info, setInfo] = useState({});
+    const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    const request = useCallback(async () => {
-        const res = await fetch(currentCharacter);
-        const nextData = await res.json();
-        const { info: nextInfo, results: nextResults = [] } = nextData;
-
-        setCharacterPage(prev => {
-            return {
-                ...prev,
-                ...nextInfo
-            }
-        });
-
-        setCharacterResults(prev => {
-            return [
-                ...prev,
-                ...nextResults
-            ]
-        });
-
-
-    }, [currentCharacter]);
 
     useEffect(() => {
-        request();
-    }, [request]);
+        setLoading(true);
+        fetch(`${baseApiEndPoint}/character/?page=${page}`)
+            .then(res => res.json())
+            .then(({info: resDataInfo, results: resDataResults}) => {
+                setInfo(resDataInfo);
+                setResults(resDataResults);
+            })
+            .catch(error => {
+                setError(error);
+                setResults([]);
+            })
+            .finally(() => setLoading(false));
+    }, [baseApiEndPoint, page]);
 
+    const characters = results.map(character => (
+        <CardDataREST key={character.id} character={character} />
+    ));
 
-    const handleLoadMore = () => {
-        setCharacterPage(prev => (
-            { ...prev, current: characterPage?.next }
-        ));
-    }
+    const handleNext = () =>  setPage(page + 1);
 
-    const charactersData = characterResults.map((character) => {
-        return <CardDataREST key={character.id} character={character} />
-    });
+    const handlePrevious = () => setPage(page - 1);
+
+    if (loading) return <h3>Loading..."</h3>
+    if (error) return <h3>Error {error.message}</h3>
 
     return <Fragment>
         <div className={classes.dataList}>
-            {charactersData}
+            {characters}
         </div>
         <div className={classes.btnWrapper}>
-            <button className={classes.loadMoreBtn} onClick={handleLoadMore}>Load More</button>
+            <button
+                className={classes.loadMoreBtn}
+                disabled={!info.prev}
+                onClick={handlePrevious}
+            >Previous
+            </button>
+            <button
+                className={classes.loadMoreBtn}
+                disabled={!info.next}
+                onClick={handleNext}
+            >Next
+            </button>
         </div>
     </Fragment>
 }
---
+++ src/components/RestLoadMore.js
@@ -0,0 +1,65 @@
+import {Fragment, useCallback, useEffect, useState} from "react";
+import CardDataREST from "./CardDataREST";
+import classes from "./DataList.module.css";
+
+const RestLoadMore = () => {
+    const defaultEndpoint = 'https://rickandmortyapi.com/api';
+    const characterEndPoint = `${defaultEndpoint}/character`;
+    const [characterResults, setCharacterResults] = useState([]);
+    const [characterPage, setCharacterPage] = useState({
+        prev: null,
+        current: characterEndPoint,
+        next: null,
+        pages: 34,
+        count: 671,
+    });
+    const { current: currentCharacter } = characterPage;
+
+    const request = useCallback(async () => {
+        const res = await fetch(currentCharacter);
+        const nextData = await res.json();
+        const { info: nextInfo, results: nextResults = [] } = nextData;
+
+        setCharacterPage(prev => {
+            return {
+                ...prev,
+                ...nextInfo
+            }
+        });
+
+        setCharacterResults(prev => {
+            return [
+                ...prev,
+                ...nextResults
+            ]
+        });
+
+
+    }, [currentCharacter]);
+
+    useEffect(() => {
+        request();
+    }, [request]);
+
+
+    const handleLoadMore = () => {
+        setCharacterPage(prev => (
+            { ...prev, current: characterPage?.next }
+        ));
+    }
+
+    const charactersData = characterResults.map((character) => {
+        return <CardDataREST key={character.id} character={character} />
+    });
+
+    return <Fragment>
+        <div className={classes.dataList}>
+            {charactersData}
+        </div>
+        <div className={classes.btnWrapper}>
+            <button className={classes.loadMoreBtn} onClick={handleLoadMore}>Load More</button>
+        </div>
+    </Fragment>
+}
+
+export default RestLoadMore;
